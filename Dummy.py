5b4f2ccf01ceb9a0d8d0c27a8d2c957d6d3592c8
DevanjanHBITS <devanjan.mukherjee@humanebits.com>
2023-03-17 17:47:45 +0530

    PlantaDoce

--- Dummy.py
+++ Dummy.py
@@ -1,8 +1,12 @@
 import mysql.connector
+import numpy.random
 import pymysql
 import pandas as pd
+from collections import namedtuple
+from dataclasses import make_dataclass
 from sqlalchemy import create_engine, text
 import datetime
+import numpy as np
 
 # mydb = mysql.connector.connect(
 #     host="localhost",
@@ -11,12 +15,13 @@ import datetime
 # )
 # mycursor = mydb.cursor()
 
-mydb_sqlalchemy = create_engine("mysql+pymysql://{user}:{pw}@localhost/sys"
-                                .format(user="root", pw="hbits"))
-data = pd.DataFrame({"ID": ['1'], 'book_name': ['XYZ'], 'book_author': ['JAMES']})
-data=data.set_index('ID',verify_integrity=True)
-print(data)
-data.to_sql('book_details', con=mydb_sqlalchemy, if_exists='append', chunksize=1000)
+# SQL Alchemy
+# mydb_sqlalchemy = create_engine("mysql+pymysql://{user}:{pw}@localhost/sys"
+#                                 .format(user="root", pw="hbits"))
+# data = pd.DataFrame({"ID": ['1'], 'book_name': ['XYZ'], 'book_author': ['JAMES']})
+# data=data.set_index('ID',verify_integrity=True)
+# print(data)
+# data.to_sql('book_details', con=mydb_sqlalchemy, if_exists='append', chunksize=1000)
 
 # AppName = 'CANKADO PRO-React Onco'
 # appID= pd.read_sql(text("""Select ID from sys.tbldigafarm WHERE AppName= :AppName;"""), params={'AppName':AppName}, con=mydb_sqlalchemy.connect())
@@ -33,9 +38,60 @@ data.to_sql('book_details', con=mydb_sqlalchemy, if_exists='append', chunksize=1
 # req = requests.get(URL)
 # soup = BeautifulSoup(req.content, 'html5lib')
 
-a = datetime.datetime(2017, 6, 21, 18, 25, 30)
-b = datetime.datetime(2017, 5, 16, 8, 21, 10)
 
-# returns a timedelta object
-c = a - b
-print('Difference: ', c)
+# DateTime Parsers
+# a = datetime.datetime(2017, 6, 21, 18, 25, 30)
+# b = datetime.datetime(2017, 5, 16, 8, 21, 10)
+#
+# # returns a timedelta object
+# c = a - b
+# print('Difference: ', c)
+
+
+# Pandas Series
+# list=list(range(1,101))
+# x= numpy.random.randint(low=10,high=1000, size=100)
+# # print(type(x))
+# # print(x)
+# # list=list(range(1,151))
+# # print(list)
+# s=pd.Series(x, index=list)
+# # print(s)
+#
+# d = {"A": 0.0, "b": 1.0, "c": 2.0, "e": 3.0}
+# print(pd.Series(d, index=["b", "c", "d", "a", "e"]))
+# print(pd.Series(5.0, index=["a", "b", "c", "d", "e"]))
+# print(s[1:50:2])
+# # print(s.index)
+# # print(s.to_numpy())
+# print(s[12])
+# s.get([101], np.nan)
+# print(s[101])
+
+d = {
+    "one": pd.Series([1.0, 2.0, 3.0], index=["a", "b", "c"]),
+    "two": pd.Series([1.0, 2.0, 3.0, 4.0], index=["a", "b", "c", "d"]),
+}
+
+df = pd.DataFrame(d, index=["d", "b", "a", "c", "e"], columns=["one", "two", "three"])
+print(df)
+print(df.index)
+
+data = [{"a": 1, "b": 2, "c": 70.0}, {"a": 5, "b": 10, "c": 20}]
+df = pd.DataFrame(data, index=['1', '2'])
+print(df)
+
+Point = namedtuple("Point", "x y")
+# Point = make_dataclass("Point", [("x", int), ("y", int)])
+df = pd.DataFrame([Point(0, 0), Point(1, 1), (2, 2)])
+print(df)
+
+iris_df = pd.read_csv("Iris.csv")
+iris_df.set_index("Id", verify_integrity=True)
+print(iris_df)
+# print(iris_df.index)
+iris_df1 = iris_df.assign(sepal_ratio=lambda x: (x["SepalWidthCm"] / x["SepalLengthCm"])).head(70)
+iris_df1.set_index("Id", verify_integrity=True)
+iris_df1=iris_df1+iris_df
+print(iris_df1.to_string())
+# print(iris_df1.index)
---
+++ PlantaDoce.py
@@ -0,0 +1,52 @@
+import time
+import dateparser
+import mysql.connector
+import pandas as pd
+import regex as re
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import requests
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="hbits"
+)
+mycursor = mydb.cursor()
+
+def PlantaDoce(SearchWord):
+    URL = "https://www.plantadoce.com/buscar.html?search_text="+SearchWord
+    req = requests.get(URL)
+    soup=BeautifulSoup(req.content, 'html5lib')
+    # print(soup.prettify())
+    print(req)
+    articles= soup.findAll("article", class_="news_list_item")
+    print(len(articles))
+    ArticlesList=[]
+    for each_article in articles:
+        title=each_article.find("div", class_="maintext").find('div', class_='title')
+        Title=title.text
+        print(Title)
+        TitleURL=URL[:-1]+title.find("a")['href']
+        print(TitleURL)
+        TitleSummary=each_article.find("div", class_="maintext").find('div', class_='text').text.rstrip().strip()
+        print(TitleSummary)
+        Date=each_article.find("div", class_="datetime").text.split(' — ')
+        print(Date)
+        ArticleDate=dateparser.parse(Date[0])
+        day = ArticleDate.day
+        month = ArticleDate.month
+        year = ArticleDate.year
+        time=Date[1]
+        vals=(Title, TitleSummary, TitleURL, day, month, year, time)
+        print(vals)
+        ArticlesList.append(vals)
+
+    return ArticlesList
+
+if __name__=="__main__":
+    SearchWord='Diabetes'
+    x=PlantaDoce(SearchWord)
+    print(x)
